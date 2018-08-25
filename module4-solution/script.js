9d72f61138a35397755a034b94d220107087239c
saniya <saniya.inam786@gmail.com>
2018-08-25 15:03:01 +0530

    adding message

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -52,6 +52,7 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
   var firstLetter = names[i].charAt(0).toLowerCase();
+
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
